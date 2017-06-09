bbac1f512266876c7b6f4eaa420b7646d4c64611
Kardo <kardoj@gmail.com>
2017-06-09 23:39:44 +0300

    Adds empty lines for readability

--- src/period.cpp
+++ src/period.cpp
@@ -29,7 +29,9 @@ Period::Period(string start_time, string end_time)
 }
 
 Period::~Period() {}
+
 bool Period::is_valid() { return valid; }
+
 unsigned Period::minutes() { return period_minutes; }
 
 void Period::diff_minutes()
