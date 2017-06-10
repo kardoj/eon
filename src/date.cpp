f572642de0d23800d79d08c67ea2cbb6a310dfce
Kardo <kardoj@gmail.com>
2017-06-10 23:28:10 +0300

    Removes quotes from datetimes

--- src/date.cpp
+++ src/date.cpp
@@ -90,6 +90,6 @@ string Date::current_date_with_time()
     sprintf(dt, "%d-%d-%d", local->tm_year + 1900, local->tm_mon + 1, local->tm_mday);
     sprintf(tme, "%d:%d:%d", local->tm_hour, local->tm_min, local->tm_sec);
 
-    return "\"" + string(dt) + "_" + string(tme) + "\"";
+    return string(dt) + "_" + string(tme);
 }
 
